63ad09d29980f45d9e3062adcfb2441bb43da405
ygrace <thecolourwings@163.com>
2021-08-30 20:04:16 +0800

    changed CardList

--- src/CardList.js
+++ src/CardList.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import Card from './Card';
 
-
-// const CardList = ({robots}) => {
-//     const cardcomponent = robots.map((user,i)=>{
-//         return (
-//             <Card 
-//             key={i} 
-//             id={robots[i].id} 
-//             name={robots[i].name} 
-//             email={robots[i].email}
-//             />
-//         );
-//     });
-//     return (
-//         <div>
-//             {cardcomponent};
-//         </div>
-//     );
-// };
-// export default CardList;
-
+  
 const CardList = ({robots}) => {
-    const cardcomponent = robots.map((user,i)=> {
+    const cardcomponent = robots.map((user,i)=>{
         return (
             <Card 
-            key={i}
+            key={i} 
             id={robots[i].id} 
             name={robots[i].name} 
-            email={robots[i].email} 
+            email={robots[i].email}
             />
         );
-        
     });
     return (
         <div>
-            {cardcomponent}
+            {cardcomponent};
         </div>
     );
-}
-
+};
 export default CardList;
+
+// const CardList = ({robots}) => {
+//     const cardcomponent = robots.map((user,i)=> {
+//         return (
+//             <Card 
+//             key={i}
+//             id={robots[i].id} 
+//             name={robots[i].name} 
+//             email={robots[i].email} 
+//             />
+//         );
+        
+//     });
+//     return (
+//         <div>
+//             {cardcomponent}
+//         </div>
+//     );
+// }
+
+// export default CardList;
